02ad36b9b8aa08422518e70f71c0050062004d4f
Fernando Zank <zank@pazinatoti.com.br>
2020-05-19 15:58:42 -0300

    Finished

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
 
@@ -20,13 +21,69 @@ interface IRequest {
 @injectable()
 class CreateProductService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+    if (!customer) {
+      throw new AppError('Customer not found');
+    }
+
+    const searchProducts = products.map(p => ({ id: p.id }));
+    const dbProducts = await this.productsRepository.findAllById(
+      searchProducts,
+    );
+
+    if (dbProducts.length !== products.length) {
+      throw new AppError("There's an invalid item on your request.");
+    }
+
+    const newQuantities: IUpdateProductsQuantityDTO[] = [];
+
+    const updateProducts = dbProducts.map(dbProduct => {
+      const orderProduct = products.find(
+        product => product.id === dbProduct.id,
+      );
+
+      if (orderProduct) {
+        if (dbProduct.quantity < orderProduct.quantity) {
+          throw new AppError(
+            `${dbProduct.name} requested quantity (${orderProduct.quantity}) is greater than your storage (${dbProduct.quantity})`,
+          );
+        }
+
+        newQuantities.push({
+          id: orderProduct.id,
+          quantity: dbProduct.quantity - orderProduct.quantity,
+        });
+
+        return {
+          ...dbProduct,
+          quantity: orderProduct.quantity,
+        };
+      }
+
+      return dbProduct;
+    });
+
+    await this.productsRepository.updateQuantity(newQuantities);
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: updateProducts.map(product => ({
+        product_id: product.id,
+        price: product.price,
+        quantity: product.quantity,
+      })),
+    });
+
+    return order;
   }
 }
 
